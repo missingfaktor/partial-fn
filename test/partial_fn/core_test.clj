ba4adadaec3a38e9dc0731496a31901fed432d43
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2014-01-03 18:13:53 +0530

    Rahul - `facts-about` macro. Making tests slightly less tedious.

--- test/partial_fn/core_test.clj
+++ test/partial_fn/core_test.clj
@@ -1,32 +1,33 @@
 (ns partial-fn.core-test
   (:use [midje.sweet]
-        [partial-fn.core]))
+        [partial-fn.core]
+        [partial-fn.test-util]))
 
 (def sample-pfun (partial-fn [x y]
-                            [3 :a] :hello
-                            [4 :b] :world))
+                             [3 :a] :hello
+                             [4 :b] :world))
 
 (def another-sample-pfun (partial-fn [x y]
-                                    :else :always))
+                                     :else :always))
 
-(facts "about partial functions"
-       (sample-pfun 3 :a) => :hello
-       (sample-pfun 4 :b) => :world
-       (sample-pfun :whoopty :do) => (throws Exception)
-       (another-sample-pfun :whoopty :do) => :always
-       (in-domain? sample-pfun 3 :a) => true
-       (in-domain? sample-pfun 4 :b) => true
-       (in-domain? sample-pfun :whoopty :do) => false
-       (in-domain? another-sample-pfun :whoopty :do) => true)
+(facts-about "partial functions"
+             (sample-pfun 3 :a) => :hello
+             (sample-pfun 4 :b) => :world
+             (sample-pfun :whoopty :do) => (throws Exception)
+             (another-sample-pfun :whoopty :do) => :always
+             (in-domain? sample-pfun 3 :a) => true
+             (in-domain? sample-pfun 4 :b) => true
+             (in-domain? sample-pfun :whoopty :do) => false
+             (in-domain? another-sample-pfun :whoopty :do) => true)
 
-(facts "about `define-partial-fn`"
-       (define-partial-fn foo [a]
-                          [:a] :ok
-                          :else :ko) =expands-to=> (def foo
-                                                     (partial-fn.core/partial-fn [a]
-                                                                                 [:a] :ok
-                                                                                  :else :ko)))
+(facts-about define-partial-fn
+             (define-partial-fn foo [a]
+                                [:a] :ok
+                                :else :ko) =expands-to=> (def foo
+                                                           (partial-fn.core/partial-fn [a]
+                                                                                       [:a] :ok
+                                                                                       :else :ko)))
 
-(facts "about `empty-partial-fn`"
-       (empty-partial-fn 4 5 6) => (throws RuntimeException)
-       (in-domain? empty-partial-fn 4 5 6) => false)
+(facts-about empty-partial-fn
+             (empty-partial-fn 4 5 6) => (throws RuntimeException)
+             (in-domain? empty-partial-fn 4 5 6) => false)
--- test/partial_fn/util_test.clj
+++ test/partial_fn/util_test.clj
@@ -1,10 +1,11 @@
 (ns partial-fn.util-test
   (:use [midje.sweet]
-        [partial-fn.util]))
+        [partial-fn.util]
+        [partial-fn.test-util]))
 
-(facts "about `make-keyword-map`"
-       (let [x 3
-             y 4
-             z :zeta]
-         (make-keyword-map x y z)) => {:x 3 :y 4 :z :zeta})
+(facts-about make-keyword-map
+             (let [x 3
+                   y 4
+                   z :zeta]
+               (make-keyword-map x y z)) => {:x 3 :y 4 :z :zeta})
 
