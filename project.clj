8bac560b07b2d142a3e85fa17ede68f4d77d0968
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2014-03-31 11:55:15 +0530

    Rahul - Moving the project to a new home. (For rebranding purposes.)

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject partial-fn "0.1.0-SNAPSHOT"
-  :description "A simple PartialFunction implementation for Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
--- src/partial_fn/util.clj
+++
@@ -1,4 +0,0 @@
-(ns partial-fn.util)
-
-(defmacro make-keyword-map [& syms]
-  `(hash-map ~@(mapcat (fn [s] [(keyword (name s)) s]) syms)))
